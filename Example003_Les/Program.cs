4f8e4cc097a4dc8a1faebb1ced75066089565f3b
ARTUR <arturpirat007@gmail.com>
2022-11-07 00:41:15 +0300

    Третья задача

---
+++ Example003_Les/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.Write("Введите число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 1 || number <= 7)
+{
+    if (number == 6 | number == 7) Console.WriteLine("Сегодня выходной");
+    else Console.WriteLine("Сегодня рабочий день");
+}
+else Console.WriteLine("Не верное число");
